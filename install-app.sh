cbd5bb9e6b1189c2bdd171782f986313da247f06
Federico Ulfo <rainelemental@gmail.com>
2016-08-14 12:59:54 -0400

    Added install files

--- install-app.sh
+++ install-app.sh
@@ -1,4 +1,13 @@
-brew cask install dropbox
-brew cask install google-chrome
-brew cask install spotify
-brew cask install sublime-text3
+# Dev Apps
+brew cask install --appdir="~/Applications" iterm2
+brew cask install --appdir="/Applications" sublime-text
+brew cask install --appdir="/Applications" pycharm
+brew cask install --appdir="/Applications" slack
+
+# Must have app
+brew cask install --appdir="/Applications" google-chrome
+brew cask install --appdir="/Applications" skype
+brew cask install --appdir="/Applications" spotify
+brew cask install --appdir="/Applications" spotify-notifications
+brew cask install --appdir="/Applications" dropbox
+brew cask install --appdir="/Applications" moom
--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -3,3 +3,4 @@ ln -s "$(pwd)/.bash_profile" ~
 ln -s "$(pwd)/.env" ~
 ln -s "$(pwd)/.inputrc" ~
 ln -s "$(pwd)/.gitignore_global" ~
+ln -s "$(pwd)/.extra" ~
